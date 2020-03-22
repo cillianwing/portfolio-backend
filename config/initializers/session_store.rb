b68cabcf760803043fde23e95d1a5a5edab47eb3
Cillian Wing <cwing@zagmail.gonzaga.edu>
2020-03-21 21:37:51 -0500

    Updated session_store.rb initializer file to handle production and development environments.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_portfolio_app", domain: "http://localhost:3000"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_portfolio_app", domain: "https://cbw-portfolio-app-react.herokuapp.com" # url is fake domain name for production app
+else
+  Rails.application.config.session_store :cookie_store, key: "_portfolio_app"
+end
