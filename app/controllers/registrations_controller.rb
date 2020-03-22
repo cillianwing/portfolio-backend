c1c22b5de3a6143e7d2ef5840fc93fd76adac1a9
Cillian Wing <cwing@zagmail.gonzaga.edu>
2020-03-21 21:34:43 -0500

    Created RegistrationsController with a create action and added necessary route resource to routes.rb - this will be used to create a new user. Also created a CurrentUserConcern to set the @current_user if one is logged in and added a logged_in action to SessionsContoller as well as a logout action to SessionController.

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationsController < ApplicationController
+
+  def create 
+    user = User.create!(user_params)
+    if user
+      session[:user_id] = user.id
+      render json: {status: :created, user: user}
+    else
+      render json: {status: 500}
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :phone_number, :street, :city, :state, :zip_code, :linkedin_url, :github_url)
+  end
+
+end
