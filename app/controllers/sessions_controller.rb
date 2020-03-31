da11fcfce62acf7fd070a0ee728a2d9b69925d22
Cillian Wing <cwing@zagmail.gonzaga.edu>
2020-03-31 14:38:14 -0500

    Removed error message from rendered json when user login fails. The user instance variable did not have access to an error method that I was calling. Will need to figure out how to return the proper error message (user not found or authentication error)  in the future.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,12 @@ class SessionsController < ApplicationController
   include CurrentUserConcern
 
   def create
-    user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
+    user = User.find_by(email: user_params[:email]).try(:authenticate, user_params[:password])
     if user 
       session[:user_id] = user.id 
       render json: {status: :created, logged_in: true, user: user}
     else 
-      render json: {status: 401, message: user.errors.full_messages}
+      render json: {status: 401}
     end
   end
 
@@ -22,6 +22,12 @@ class SessionsController < ApplicationController
   def logout
     reset_session
     render json: {status: 200, logged_out: true}
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 
 end
